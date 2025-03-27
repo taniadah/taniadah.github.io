f663d5de8519cf8099c3a5d0e44d28e2f1cefe63
taniadah <taniadelangelh@gmail.com>
2025-03-27 15:52:30 -0600

    Portafolio actualizado 2025

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 Autor: Tania Del Angel 
 Date: 17.01.2024
 */
-
+let index = 0;
 
 (function (){
   "use strict";
@@ -178,3 +178,20 @@ function putAge(){
   }   
   object = document.getElementById('Edad').innerHTML = años.toString()+" años";
 }
+
+
+
+    function moveSlide(step) {
+        const images = document.querySelectorAll(".carousel-images img");
+        const totalImages = images.length;
+
+        index += step;
+
+        if (index < 0) {
+            index = totalImages - 1;
+        } else if (index >= totalImages) {
+            index = 0;
+        }
+
+        document.querySelector(".carousel-images").style.transform = `translateX(-${index * 100}%)`;
+    }
