e77974bbb05d1f09e2cde7e1f30dd32efeedb4e5
taniadah <taniadelangelh@gmail.com>
2026-09-09 20:03:05 -0600

    feat(taller): improve experience card layout and navigation

---
+++ js/constants/constanst_taller.js
@@ -0,0 +1,26 @@
+export const TECHNOLOGIES_PER_PAGE = 4;
+export const COMPANIES_ICONS = new Map([
+  ["rica", "../../assets/icons/coca_icon.png"],
+  ["bluetab", "../../assets/icons/bluetab_exp.png"],
+]);
+export const TECHNOLOGY_ICONS = new Map([
+    ["Apex", "../../assets/icons/apex_card.png"],
+    ["Aura", "../../assets/icons/aura_card.png"],
+    ["C#", "../../assets/icons/csharp_card.png"],
+    ["CSS", "../../assets/icons/css_card.png"],
+    ["Flows", "../../assets/icons/flows_card.png"],
+    ["Git", "../../assets/icons/git_card.png"],
+    ["HTML", "../../assets/icons/html_card.png"],
+    ["Java", "../../assets/icons/java_card.png"],
+    ["JavaScript", "../../assets/icons/js_card.png"],
+    ["LWC", "../../assets/icons/lwc_card.png"],
+    ["MySQL", "../../assets/icons/mysql_card.png"],
+    ["PHP", "../../assets/icons/php_card.png"],
+    ["Python", "../../assets/icons/python_card.png"],
+    ["Salesforce", "../../assets/icons/salesforce_card.png"],
+    ["SOQL", "../../assets/icons/soql_card.png"],
+    ["SQL Server", "../../assets/icons/sqlserver_card.png"],
+    ["SQLite", "../../assets/icons/soql_card.png"],
+    ["Windows Server", "../../assets/icons/ws_card.png"],
+    ["Xamarin", "../../assets/icons/xamarin_card.png"],
+]);
--- js/pages/taller.js
+++ js/pages/taller.js
@@ -1,28 +1,78 @@
+import {
+    TECHNOLOGIES_PER_PAGE,
+    COMPANIES_ICONS,
+    TECHNOLOGY_ICONS
+} from "../constants/constanst_taller.js";
+
 const companies = document.querySelectorAll(".trajectory-company");
-const workshop = document.querySelector(".workshop-container");
 const experienceView = document.querySelector(".experience-view");
-const experienceCompany = document.querySelector(".experience-company");
-const experienceStatus = document.querySelector(".experience-status");
+const companyName = document.querySelector(".company-infoname");
+const companyPeriod = document.querySelector(".company-infoperiod");
+const companyIconCard = document.querySelector(".company-iconcard");
+const aboutContent = document.getElementById("about-content");
+const grownContent = document.getElementById("learning-content");
+const rolesContent = document.querySelector(".roles-content");
+const timeline = document.querySelector(".experience-timeline");
 
 fetch("../../data/experiences.json")
     .then(response => response.json())
     .then(experiences => {
+        console.log(experiences);
+        buildTimeline(experiences);
         companies.forEach(company => {
-            company.addEventListener("click", () => {
-                const companyData = experiences[company.dataset.company];
-                console.log(companyData);
-                workshop.classList.add("zooming");
+            company.addEventListener("click", () => {               
+                const companyData = experiences[company.dataset.company];                
                 experienceView.hidden = false;
                 setTimeout(() => {
-    experienceView.classList.add("visible");
-}, 300);
+                    experienceView.classList.add("visible");
+                }, 300)
+                fillExperience(companyData,company.dataset.company);
             });
         });
     });
 
-function fillExperience(company, status) {
+function fillExperience(companyData, compName) {
+    console.log(companyData)
+    companyName.textContent = companyData.company;
+    companyPeriod.textContent = companyData.period;
+    aboutContent.textContent = companyData.about;
+    grownContent.textContent = companyData.learning;
+    console.log( COMPANIES_ICONS.get(compName));
+    companyIconCard.src = COMPANIES_ICONS.get(compName);
+    rolesContent.innerHTML = "";
+    companyData.roles.forEach(role => {
+    const roleBlock = document.createElement("div");
+    roleBlock.classList.add("role-block");
+    roleBlock.innerHTML = `
+        <h4 class="role-title">${role.title}</h4>
+        <ul class="activities-list">
+            ${role.activities
+                .map(activity => `<li>${activity}</li>`)
+                .join("")}
+        </ul>
+    `;
+    rolesContent.appendChild(roleBlock);
+    });
+}
 
-    experienceCompany.textContent = company;
-    experienceStatus.textContent = status;
-
+function buildTimeline(experiences) {
+    timeline.innerHTML = "";
+    Object.entries(experiences).forEach(([key, companyData], index) => {
+        const item = document.createElement("div");
+        item.classList.add("timeline-item");
+        item.dataset.company = key;
+        item.innerHTML = `
+            <div class="timeline-node">
+                <img 
+                    src="${COMPANIES_ICONS.get(key)}" 
+                    alt="${companyData.company}"
+                >
+            </div>
+            <div class="timeline-info">
+                <h3>${companyData.company}</h3>
+                <p>${companyData.period}</p>
+            </div>
+        `;
+        timeline.appendChild(item);
+    });
 }
